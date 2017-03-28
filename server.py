c95756486fc4d3db973bc130550bbbb3a0acf43d
ma306857 <abdou.mhalidi@gmail.com>
2017-03-28 22:48:03 +0200

    creation fichier server.py et debut implemenation du code

--- server.py
+++ server.py
@@ -3,34 +3,6 @@
 
 import socket, sys,os,signal,time
 
-print("A tout moment appuyé sur CTRL+C pour fermer le serveur, Merci :) ")
-
-#création du fichier historique_activite.log
-historique_activite = os.open("historique_activite.log", os.O_APPEND|os.O_CREAT|os.O_WRONLY) # créer le fichier et ajoute à la fin 
-
-def log_historique(message):
-    '''
-    La fonction log succes permet d'ecrire un message de succès dans notre fichier succes.log
-    '''
-    os.write(historique_activite, time.strftime("%A %d %B %Y %H:%M:%S") + " " + message + "\n")
-
-# Le traitant
-def traitant(signal, frame):
-    '''
-    Le traitant permet de cloturer notre serveur avec CTRL+C
-    '''
-    os.close(historique_activite) #fermeture du fichier succes.log
-    server.close()
-
-#ici on souhaite attraper le signal CTRL C à l'aide du traitant
-signal.signal(signal.SIGINT, traitant)
-
-
-'''
-        PARTIE PRINCIPAL DU CLIENT 
-'''
-
-
 '''
         PARTIE DU SERVEUR 
 '''
@@ -43,7 +15,6 @@ server.bind((host,port)) #association host et du port
 server.listen(10)
 liste_de_connection.append(server)
 running = 1
-
 
 while running:
         inputready,outputready,exceptready = select.select(liste_de_connection,[],[])
